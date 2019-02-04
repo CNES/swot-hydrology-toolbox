minor fix in dark water (to use old gaussian model, to be changed) and in area computation

sisimp/lib/dark_water_functions.py
@@ -26,7 +26,7 @@ def dark_water_simulation(taille_2D, fact_echelle_dw, pourcent_dw, seedvalue=Non
     # Create a 2D gaussian profile
     if seedvalue is not None:
         np.random.seed(int(seedvalue))
-    profile_2d = height_model.generate_2d_profile_gaussian(taille_2D, 0., 'Default', 1, fact_echelle_dw)
+    profile_2d = height_model.generate_2d_profile_gaussian_old(taille_2D, 0., 'Default', 1, fact_echelle_dw)
     
     ### Create Dark water mask
     # Define the threshold value to keep the desired % of dark water
@@ -85,7 +85,7 @@ def dark_water_non_detected_simulation(mask_dw,fact_echelle_nondetected_dw,perce
             size_y=maxy-miny+1
 
             #Simulate non detected dark water
-            profile_2d = height_model.generate_2d_profile_gaussian([size_x,size_y],0.,'Default',1,fact_echelle_nondetected_dw)
+            profile_2d = height_model.generate_2d_profile_gaussian_old([size_x,size_y],0.,'Default',1,fact_echelle_nondetected_dw)
             profile_2d[np.where(mask_regions[minx:maxx+1,miny:maxy+1]!=i)]=-999
             non_detected_dw_mask[minx:maxx+1,miny:maxy+1]=profile_2d
         # Define the threshold value to keep the percentage of non_detected dark water inside detected dark_water regions

sisimp/write_polygons.py
@@ -617,9 +617,9 @@ def reproject_shapefile(IN_filename, IN_swath, IN_driver, IN_attributes, IN_cycl
                     continue  # ignore polygons completely outside the swath
 
                 points = np.transpose(np.array(ring.GetPoints()))
-                lon = points[0] * DEG2RAD
-                lat = points[1] * DEG2RAD
-               
+                
+                lon = points[0]
+                lat = points[1]
                
                 x_c, y_c, zone_number, zone_letter = utm.from_latlon(lat[0], lon[0])
                 latlon = pyproj.Proj(init="epsg:4326")
@@ -631,9 +631,14 @@ def reproject_shapefile(IN_filename, IN_swath, IN_driver, IN_attributes, IN_cycl
                     ring_xy.AddPoint(X[i],Y[i])
                 poly_xy = ogr.Geometry(ogr.wkbPolygon)
                 poly_xy.AddGeometry(ring_xy)
-                area = poly_xy.GetArea()
+                
+                # area in ha
+                area = poly_xy.GetArea()/10000.
            
                 layerDefn = layer.GetLayerDefn()
+                
+                lon = lon * DEG2RAD
+                lat = lat * DEG2RAD
                 
                 lac = None
                 
@@ -645,7 +650,6 @@ def reproject_shapefile(IN_filename, IN_swath, IN_driver, IN_attributes, IN_cycl
                 
                 if IN_attributes.height_model == 'gaussian': 
                     if area > IN_attributes.height_model_min_area:
-                        print(ind+1)
                         my_api.printInfo(str("Gaussian model applied for big water body of size %f ha" % area))
                         lac = Gaussian_Lac(ind+1, IN_attributes, lat, lon, IN_cycle_number)
                     else: