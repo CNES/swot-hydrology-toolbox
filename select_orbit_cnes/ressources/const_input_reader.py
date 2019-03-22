8bce08d0598d0004313dd057714e2c3b7ec9b19c
Claire Pottier <claire.pottier@cnes.fr>
2019-03-22 14:06:53 +0100

    [select_orbit] [BUG] "Azimuth spacing" configuration parameter was neither read nor used: corrected

--- select_orbit_cnes/ressources/const_input_reader.py
+++ select_orbit_cnes/ressources/const_input_reader.py
@@ -21,7 +21,7 @@ from __future__ import division
 # Error messages
 
 # log messages
-TRACE_START_READING_PARAM = "Starting reading global parameter file.."
+TRACE_START_READING_PARAM = "Starting reading global parameter file."
 TRACE_END_READING_PARAM = "The global parameter file was successfully read."
 
 # RDF parameter
@@ -31,12 +31,13 @@ LON_WEST = "DEM west longitude"
 LON_EAST = "DEM east longitude"
 LAT_SOUTH = "DEM south latitude"
 LAT_NORTH = "DEM north latitude"
+AZIMUTH_SPACING = "Azimuth spacing"
 SWATH = "Swath width"
 NEAR_RANGE = "NR cross track"
 GDEM_PREFIX = "GDEM Orbit prefix"
 ORBIT_REPOSITORY = "Orbit repository"
 START_MISSION_TIME = "Mission start time"
+CYCLE_DURATION = "Cycle duration"
 MAKE_PASS_PLAN = "passplan"
 SIMULATION_START = "simulation_start_time"
 SIMULATION_STOP = "simulation_stop_time"
-CYCLE_DURATION = "Cycle duration"
